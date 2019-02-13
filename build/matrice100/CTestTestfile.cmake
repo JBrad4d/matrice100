5a6798fb2f5d17ab4b4d2f9f17ac607ee3ae7391
JBrad4d <jbradford4@aggiemail.usu.edu>
2019-02-12 22:30:24 -0700

    first

---
+++ build/matrice100/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kcarl/git/matrice100/src/matrice100
+# Build directory: /home/kcarl/git/matrice100/build/matrice100
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
